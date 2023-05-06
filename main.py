eef089b37bfe1c244aa032f7915ee3618650a2a4
Lailai <Lailai@gmail.com>
2023-05-06 23:05:24 +0800

    fix create db3

--- main.py
+++ main.py
@@ -27,8 +27,6 @@ Base = declarative_base()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
 
 #FLASK_Login
 login_manager = LoginManager()
@@ -269,4 +267,6 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000,debug=True)
